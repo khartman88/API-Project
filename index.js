fb5ca0d21092b89ede32a28241ac5c35dcfda28e
Kyle Hartman <khartman@nhcgov.com>
2025-02-09 12:07:15 -0500

    api updates final

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 
-fetch("https://api.open-meteo.com/v1/forecast?latitude=40.7128&longitude=-74.0060&current_weather=true")
-    .then(response => {
-        if (!response.ok) {
-            console.log("Response status:", response.status); //checking fetch
-        throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    return response.json();
-})
-    .then(data => {
-        console.log('Weather Data:', data);
-    
-        const temperature = data.current_weather.temperature;
-        const weatherElement = document.getElementById("weather");
-        weatherElement.innerText = `Current Temperature: ${temperature}°C`;
-      })
-      .catch(error => {
-        console.error("Error fetching weather data:", error);
-      });
+const weatherElement = document.getElementById('weather');
+const temperatureButton = document.getElementById('temperatureButton');
+const windButton = document.getElementById('windButton');
+
+// fetches //
+
+const fetchWeatherData = (endpoint) => {
+    fetch("https://api.open-meteo.com/v1/forecast?latitude=40.7128&longitude=-74.0060&current_weather=true")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+            return response.json();
+    })
+        .then(data => {
+            if (endpoint === 'temperature') {
+                weatherElement.innerText = `Current Temperature: ${data.current_weather.temperature}°C`;
+            } else if (endpoint === 'wind') {
+                weatherElement.innerText = `Wind Speed: ${data.current_weather.windspeed} km/h`;
+            }
+    })
+};
+
+// button events //
+
+temperatureButton.addEventListener('click', () => fetchWeatherData('temperature'));
+windButton.addEventListener('click', () => fetchWeatherData('wind'));
+
+// default temp //
+fetchWeatherData('temperature');
 
